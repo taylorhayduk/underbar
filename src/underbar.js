58cead589a2c0039a3af0e0f9fdcfa77b9d99d3e
Taylor Hayduk <taylorhayduk@gmail.com>
2015-07-31 11:05:34 -0400

    Passed All: Extend

--- src/underbar.js
+++ src/underbar.js
@@ -241,6 +241,13 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    for (var i = 1; i < arguments.length; i++) {
+      var currentObj = arguments[i];
+      _.each(currentObj, function(v, k) {
+        obj[k] = v;
+      })
+    }
+    return obj;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
